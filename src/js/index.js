95b2bf2302fb5d249ef9bc386bd64a42de6a79b5
Adeen <adeenkhoso@gmail.com>
2023-12-10 23:15:06 +0500

    refactored header code

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,20 @@
-      // Selecting elements
-      navbar = document.querySelector('.navigation');
-      hamburger = document.querySelector('.hamburger');
-      logo = document.querySelector('.logo');
+
+     // Selecting elements
+      const navbar = document.querySelector('.navigation');
+      const navLinks = document.querySelectorAll('.navigation a');
+      const hamburger = document.querySelector('.hamburger');
+      const logo = document.querySelector('.logo');
       
       // Adding click event listener to hamburger
       hamburger.onclick = function() {
         navbar.classList.toggle('active');
         logo.classList.toggle('active');
       };
+
+      // Adding click event listeners to each link
+      navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+          navbar.classList.remove('active');
+          logo.classList.remove('active');
+        });
+      });
