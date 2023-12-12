df7c137c61fb22a382ac9fc089dfa97959c9f633
Zain <zain.khoso.dev@gmail.com>
2023-12-12 15:17:16 +0500

    Small code refactor

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,19 @@
-// const navBtn = document.getElementById('nav-tog-btn');
+// Selecting elements
+const navbar = document.querySelector(".navigation");
+const navLinks = document.querySelectorAll(".navigation a");
+const hamburger = document.querySelector(".hamburger");
+const logo = document.querySelector(".logo");
 
-// navBtn.addEventListener("click",navActive())
-// const navActive = function(){
-//     navBtn.classList.toggle('nav-tog-btn')
-// }
+// Adding click event listener to hamburger
+hamburger.onclick = function () {
+    navbar.classList.toggle("active");
+    logo.classList.toggle("active");
+};
+
+// Adding click event listeners to each link
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+        navbar.classList.remove("active");
+        logo.classList.remove("active");
+    });
+});
